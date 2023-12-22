e68d5013d7f4dd33ed874fb36f80bf33c1a61764
AntonioIlinovic <antonio.ilinovic1@gmail.com>
2023-12-22 13:19:55 +0100

    prikaz User stranice kad korisnik nije ulogiran

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,6 +6,9 @@ import { motion } from 'framer-motion';
 function User() {
     const { user } = useContext(Contextpage);
 
+    // Check if user is logged in
+    const isLoggedIn = !!user; // Converts user to a boolean value
+
     return (
         <>
             <div className='w-full bg-[#10141e] md:p-10 mb-20 md:mb-0'>
@@ -15,14 +18,20 @@ function User() {
                     className="w-full md:p-2 flex flex-wrap relative justify-evenly md:justify-around">
                     <div className="text-white text-center">
                         <h1 className="text-2xl font-bold">User Info</h1>
-                        <p className="text-lg">Email: {user.email}</p>
-                        <p className="text-lg">Name: {user.displayName || 'N/A'}</p>
-                        {user.photoURL && (
-                            <img
-                                src={user.photoURL}
-                                alt="user"
-                                className="w-40 h-40 rounded-full mx-auto"
-                            />
+                        {isLoggedIn ? (
+                            <>
+                                <p className="text-lg">Email: {user.email}</p>
+                                <p className="text-lg">Name: {user.displayName || 'N/A'}</p>
+                                {user.photoURL && (
+                                    <img
+                                        src={user.photoURL}
+                                        alt="user"
+                                        className="w-40 h-40 rounded-full mx-auto"
+                                    />
+                                )}
+                            </>
+                        ) : (
+                            <p className="text-lg">You are not logged in.</p>
                         )}
                     </div>
                 </motion.div>
